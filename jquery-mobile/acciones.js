31a855a2765b8afe91a2587048dbd5f0f13aba0b
Eugenia36 <beilisita_1717@hotmail.com>
2015-06-08 16:54:14 -0700

    anima1
    ver 0.1

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function(){
+	var pollo = document.getElementById("pollo");
+	var gato = document.getElementById("gato");
+	var pajaro = document.getElementById("pajaro");
+	var pato = document.getElementById("pato");
+	var vaca = document.getElementById("vaca");
+	var perro = document.getElementById("perro");
+	
+	var elefante = document.getElementById("elefante");
+	var mono = document.getElementById("mono");
+	var hipopotamo = document.getElementById("hipopotamo");
+	var tigre = document.getElementById("tigre");
+	var serpiente = document.getElementById("serpiente");
+	var oso = document.getElementById("oso");
+});
